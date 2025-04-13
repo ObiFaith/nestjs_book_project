cd44006379d29658ad4571c82e082b48b203c6b6
ObiFaith <obifth@gmail.com>
2025-04-13 15:38:49 +0100

    feat: config helmet, rate-limit and cors to app

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,33 @@
+import helmet from 'helmet';
 import { AppModule } from './app.module';
 import { NestFactory } from '@nestjs/core';
+import rateLimit from 'express-rate-limit';
 import { ValidationPipe } from '@nestjs/common';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // Enable CORS
+  app.enableCors({
+    origin: ['localhost:3000'],
+    methods: ['GET', 'POST', 'PATCH', 'DELETE'],
+    credentials: true,
+  });
+
+  // Use Helmet for security
+  app.use(helmet());
+
+  // Apply rate limiting
+  app.use(
+    rateLimit({
+      max: 100,
+      windowMs: 15 * 60 * 1000,
+      message: 'Maximum Requests has been excedeed, try again in 10 minutes!',
+    }),
+  );
+
+  // Config Swagger for api doc
   const config = new DocumentBuilder()
     .setTitle('Book API Project')
     .setDescription('Book API CRUD')
@@ -18,4 +41,6 @@ async function bootstrap() {
   await app.listen(process.env.PORT ?? 3000);
 }
 
-bootstrap();
+bootstrap().catch((err) => {
+  console.error('Error during bootstrap:', err);
+});
